bc36fcdd6b552e51afcb4fd2ed90796d3dedf606
YashN2004 <149130596+YashN2004@users.noreply.github.com>
2025-04-02 13:35:07 +0530

    Update Assignment 4-DES.py

--- ASSIGNMENT 4(DES AND AES)/Assignment 4-DES.py
+++ ASSIGNMENT 4(DES AND AES)/Assignment 4-DES.py
@@ -1,24 +1,18 @@
 from Crypto.Cipher import DES
-from secrets import token_bytes
-key = token_bytes(8)
-def encrypt(msg):
-    cipher = DES.new(key, DES.MODE_EAX)
-    nonce = cipher.nonce
-    ciphertext, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
-    return nonce, ciphertext, tag
-def decrypt(nonce, ciphertext, tag):
-    cipher = DES.new(key, DES.MODE_EAX, nonce=nonce)
-    plaintext = cipher.decrypt(ciphertext)
-    try:
-        cipher.verify(tag)
-        return plaintext.decode('ascii')
-    except:
-        return False
-        
-nonce, ciphertext, tag = encrypt(input('Enter a message: '))
-plaintext = decrypt(nonce, ciphertext, tag)
-print(f'Cipher text: {ciphertext}')
-if not plaintext:
-    print('Message is corrupted!')
-else:
-    print(f'Plain text: {plaintext}')
+from secrets import token_bytes 
+ 
+
+key = token_bytes(8)  
+cipher = DES.new(key, DES.MODE_EAX)
+
+data = input("Enter the data to encrypt: ").encode()  
+
+nonce = cipher.nonce
+ciphertext, tag = cipher.encrypt_and_digest(data) 
+
+print("Cipher text:", ciphertext)
+
+cipher = DES.new(key, DES.MODE_EAX, nonce=nonce)
+plaintext = cipher.decrypt(ciphertext)
+
+print("Plain text:", plaintext.decode())
