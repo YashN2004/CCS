9787f09a11b761c38ee67e7906c89888e20670f7
YashN2004 <149130596+YashN2004@users.noreply.github.com>
2025-04-02 13:35:37 +0530

    Update Assignment 4-AES.py

--- ASSIGNMENT 4(DES AND AES)/Assignment 4-AES.py
+++ ASSIGNMENT 4(DES AND AES)/Assignment 4-AES.py
@@ -1,10 +1,18 @@
 from Crypto.Cipher import AES
-key = b'C&F)H@McQf9TjWnZ'  
+from secrets import token_bytes 
+
+key = token_bytes (16)  
 cipher = AES.new(key, AES.MODE_EAX)
-data = "Hello World".encode()
+
+
+data = input("Enter the data to encrypt: ").encode()  
+
 nonce = cipher.nonce
-ciphertext = cipher.encrypt(data)
+ciphertext, tag = cipher.encrypt_and_digest(data) 
+
 print("Cipher text:", ciphertext)
+
 cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
 plaintext = cipher.decrypt(ciphertext)
-print("Plain text:", plaintext)
+
+print("Plain text:", plaintext.decode()) 
